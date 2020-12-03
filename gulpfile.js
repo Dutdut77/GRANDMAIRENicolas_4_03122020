4c01ea8ca7baaf5810aa463c5793b6375bc02bda
Nicolas GRANDMAIRE <dutdut77@hotmail.fr>
2020-12-03 22:06:05 +0100

    gulp start

--- gulpfile.js
+++ gulpfile.js
@@ -7,55 +7,28 @@ const rename = require ("gulp-rename");
 const fileinclude = require("gulp-file-include");
 
 
-function makeCss(){
-  return gulp.src("./src/scss/style.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./www/css/"));
-}
 
-function prefix(){
-  return gulp.src("./www/css/style.css")
-    .pipe(autoprefixer({
-            cascade: false
-}))
-    .pipe(gulp.dest("./www/css/"));
-}
 
 function minCss(){
-  return gulp.src("./www/css/style.css")
+  return gulp.src("./style.css")
     .pipe(cleanCSS())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("./www/css/"));
+    .pipe(gulp.dest("./"));
 }
 
 
-function include(){
-  return gulp.src("./src/*.html")
-    .pipe(fileinclude({
-      prefix : "@@",
-      basepath: "@file"
-    }))
-    .pipe(gulp.dest("./www/"));
-}
 
 
 function watch(){
   browser.init({
     server: {
-      baseDir : "./www"
+      baseDir : "./"
     }
   });
-  gulp.watch("./src/scss/**/*.scss", seqdev);
-  gulp.watch("./src/*.html", include);
-  gulp.watch("./src/partial/*.html", include);
-  gulp.watch("./www/").on("change", browser.reload);
+  gulp.watch("style.css", minCss);
+  gulp.watch("*.*").on("change", browser.reload);
 }
 
 
-const seqdev = gulp.series(makeCss, minCss);
-exports.makeCss = makeCss;
-exports.prefix = prefix;
 exports.minCss = minCss;
-exports.seqdev = seqdev;
-exports.include = include;
 exports.watch = watch; 
